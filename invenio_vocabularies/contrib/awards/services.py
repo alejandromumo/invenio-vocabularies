02c8318288d148184dc7ca809f551571d5a5f106
Manuel Alejandro <mumox7@gmail.com>
2022-03-14 12:46:13 +0100

    awards: added service.

---
+++ invenio_vocabularies/contrib/awards/services.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Vocabulary grants."""
+
+
+from .grants import record_type
+
+GrantsServiceConfig = record_type.service_config_cls
+
+GrantsService = record_type.service_cls
